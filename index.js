8f97171c003a224d713bdd9ce72b866f804cec65
Gianfranco Nuschese <gianfranconuschese@gmail.com>
2019-09-04 15:36:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+  static titleize(str){
+    const words = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    const arr = str.split(' ');
+    let newStr = '';
+    newStr += this.capitalize(arr[0]);
+    arr.shift();
+    arr.forEach(function(duh){
+      console.log(duh, words.includes(duh))
+      if (words.includes(duh) === false){
+        const newWord = Formatter.capitalize(duh);
+        newStr += " " + newWord;
+      } else{
+        newStr += " " + duh;
+      }
+      
+    })
+    return newStr;
+  }
+
 }
