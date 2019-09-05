3884ba0d9ad891dba50e25254cc5bd6b0c875eab
Janet Mendez <janetmendez.03@gmail.com>
2019-09-04 23:17:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1)
+  }
+  static sanitize(str){
+    return str.replace(/[^A-Za-z0-9-'\s]+/g, '')
+  }
+  static titleize(str){
+    const ignore = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    let newStr = str.split(' ')
+    newStr = newStr.map((letter) => {
+      
+      if (ignore.indexOf(letter) > -1 && newStr[0] !== letter) {
+        return letter
+      }
+      else {
+        return Formatter.capitalize(letter)
+      }
+    }).join(' ')
+
+    return newStr
+  }
 }
